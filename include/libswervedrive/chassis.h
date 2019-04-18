ab627c70eab6dc5b1ab86b685549560926bc99b7
Arthur Allshire <arthur@allshire.org>
2019-04-18 14:46:49 -0400

    Change to vectors in chassis
    VectorXd is already a column vector, so using them should work fine

--- include/libswervedrive/chassis.h
+++ include/libswervedrive/chassis.h
@@ -15,7 +15,8 @@ using Epsilon = Eigen::Vector3d;
 using Lambda = Eigen::Vector3d;
 
 struct Chassis {
-    Chassis(Eigen::VectorXd alpha,
+    Chassis(
+        Eigen::VectorXd alpha,
         Eigen::VectorXd l,
         Eigen::VectorXd b,
         Eigen::VectorXd r,
@@ -25,22 +26,25 @@ struct Chassis {
         Bounds phi_dot_bounds,
         Bounds phi_2dot_bounds
         ):
+        alpha(alpha), l(l), b(b), r(r),
         beta_dot_bounds(beta_dot_bounds), beta_2dot_bounds(beta_2dot_bounds),
-        phi_dot_bounds(phi_dot_bounds), phi_2dot_bounds(phi_2dot_bounds) {
-            // TODO: initialisation of matrices
-        };
+        phi_dot_bounds(phi_dot_bounds), phi_2dot_bounds(phi_2dot_bounds)
+    { 
+        n = alpha.size()(0);
+        using ModuleParam = Eigen::Matrix<double, this->n, 1>
+    };
 
     // number of wheels
     int n;
     // array containing the angle to each of the modules
     // measured counter clockwise from the x-axis in radians
-    Eigen::MatrixXd alpha;
+    Eigen::VectorXd alpha;
     // horizontal distance from the origin to the axis of rotation of each wheel
-    Eigen::MatrixXd l;
+    Eigen::VectorXd l;
     //  horizontal distance from the axis of rotation of each module to its contact with the gound
-    Eigen::MatrixXd b;
+    Eigen::VectorXd b;
     // radii of the wheels (m)
-    Eigen::MatrixXd r;
+    Eigen::VectorXd r;
     // Min/max allowable value for steering angle, in rad.
     Bounds beta_bounds;
     // Min/max allowable value for rotation rate of modules, in rad/s
