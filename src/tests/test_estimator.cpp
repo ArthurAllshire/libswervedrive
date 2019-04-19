18b628fa9fa9d487bf6102938cc6043e64e68741
James Ward <james@thedropbears.org.au>
2019-04-20 00:17:04 +1000

    Create basic test for estimator lambda conversion

---
+++ src/tests/test_estimator.cpp
@@ -0,0 +1,23 @@
+#include <gtest/gtest.h>
+
+#include "libswervedrive/chassis.h"
+#include "libswervedrive/estimator.h"
+
+TEST(EstimatorTest, ConvertsLambdaToQ) {
+  using namespace Eigen;
+  using namespace swervedrive;
+  VectorXd alpha = VectorXd::Zero(4);
+  VectorXd l = VectorXd::Constant(4, 1);
+  VectorXd b = VectorXd::Constant(4, 1);
+  VectorXd r = VectorXd::Constant(4, 1);
+  Bounds beta_bounds, beta_dot_bounds, beta_2dot_bounds, phi_dot_bounds, phi_2dot_bounds;
+
+  Chassis c(alpha, l, b, r,
+      beta_bounds, beta_dot_bounds, beta_2dot_bounds, phi_dot_bounds, phi_2dot_bounds);
+  Estimator e(c);
+
+  auto q = e.S(Lambda(0, 0, 1));
+  EXPECT_EQ(q.size(), 4);
+  VectorXd expected = VectorXd::Zero(4);
+  EXPECT_TRUE(q.isApprox(expected)) << "Expected: " << expected << " Calculated: " << q;
+}
