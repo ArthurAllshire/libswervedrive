d1cfa828e47ab2470ab0ca0c2e77fe87f2ee6473
Arthur Allshire <arthur@allshire.org>
2019-04-19 18:36:29 -0400

    Add dOxygen documentation

--- include/libswervedrive/chassis.h
+++ include/libswervedrive/chassis.h
@@ -7,63 +7,83 @@
 
 namespace swervedrive {
 
-// velocity bounds
+//! Velocity bounds
 using Bounds = std::vector<Eigen::Vector2d>;
-// position of the robot in terms of [x, y, theta]
+//! Position of the robot in terms of [x, y, theta]
 using Epsilon = Eigen::Vector3d;
-// icr of the robot in terms of the spherical parametrisation (u, v, w)
+//! ICR of the robot in terms of the spherical parametrisation (u, v, w)
 using Lambda = Eigen::Vector3d;
 
+/**
+ * @brief Represents a chassis configurtaion
+ * 
+ */
 struct Chassis {
-    Chassis(
-        Eigen::VectorXd alpha,
-        Eigen::VectorXd l,
-        Eigen::VectorXd b,
-        Eigen::VectorXd r,
-        Bounds beta_bounds,
-        Bounds beta_dot_bounds,
-        Bounds beta_2dot_bounds,
-        Bounds phi_dot_bounds,
-        Bounds phi_2dot_bounds
-        ):
-        alpha(alpha), l(l), b(b), r(r),
-        beta_dot_bounds(beta_dot_bounds), beta_2dot_bounds(beta_2dot_bounds),
-        phi_dot_bounds(phi_dot_bounds), phi_2dot_bounds(phi_2dot_bounds)
-    {
-        n = alpha.size();
-        a = Eigen::MatrixXd(3, n);
-        a << cos(alpha.array()).transpose(), sin(alpha.array()).transpose(), Eigen::VectorXd::Zero(n).transpose();
-        a_orth = Eigen::MatrixXd(3, n);
-        a_orth << -sin(alpha.array()).transpose(), cos(alpha.array()).transpose(), Eigen::VectorXd::Zero(n).transpose();
-        l_v = Eigen::MatrixXd(3, n);
-        l_v << Eigen::MatrixXd::Zero(2, n), l.transpose();
-    };
+  /**
+   * @brief Construct a new Chassis object
+   * 
+   * @param Alpha array containing the angle to each of the modules measured counter clockwise from the x-axis in radians
+   * @param l Discance from the centre of the robot to each module's steering axis, in m
+   * @param b Horizontal distance from the axis of rotation of each module to its contact with the gound, in m
+   * @param r Radii of the wheels, in m
+   * @param beta_bounds Min/max allowable value for steering angle, in rad.
+   * @param beta_dot_bounds Min/max allowable value for rotation rate of modules, in rad/s
+   * @param beta_2dot_bounds Min/max allowable value for the angular acceleration of the modules, in rad/s^2.
+   * @param phi_dot_bounds Min/max allowable value for rotation rate of module wheels, in rad/s
+   * @param phi_2dot_bounds  Min/max allowable value for the angular acceleration of the module wheels, in rad/s^2.
+   */
+  Chassis(
+    Eigen::VectorXd alpha,
+    Eigen::VectorXd l,
+    Eigen::VectorXd b,
+    Eigen::VectorXd r,
+    Bounds beta_bounds,
+    Bounds beta_dot_bounds,
+    Bounds beta_2dot_bounds,
+    Bounds phi_dot_bounds,
+    Bounds phi_2dot_bounds
+    ):
+    alpha(alpha), l(l), b(b), r(r),
+    beta_dot_bounds(beta_dot_bounds), beta_2dot_bounds(beta_2dot_bounds),
+    phi_dot_bounds(phi_dot_bounds), phi_2dot_bounds(phi_2dot_bounds)
+  {
+    n = alpha.size();
+    a = Eigen::MatrixXd(3, n);
+    a << cos(alpha.array()).transpose(), sin(alpha.array()).transpose(), Eigen::VectorXd::Zero(n).transpose();
+    a_orth = Eigen::MatrixXd(3, n);
+    a_orth << -sin(alpha.array()).transpose(), cos(alpha.array()).transpose(), Eigen::VectorXd::Zero(n).transpose();
+    l_v = Eigen::MatrixXd(3, n);
+    l_v << Eigen::MatrixXd::Zero(2, n), l.transpose();
+  };
 
-    // number of wheels
-    int n;
-    // array containing the angle to each of the modules
-    // measured counter clockwise from the x-axis in radians
-    Eigen::VectorXd alpha;
-    // horizontal distance from the origin to the axis of rotation of each wheel
-    Eigen::MatrixXd a;
-    Eigen::MatrixXd a_orth;
-    Eigen::MatrixXd l_v;
+  //! Number of wheels
+  int n;
 
-    Eigen::VectorXd l;
-    //  horizontal distance from the axis of rotation of each module to its contact with the gound
-    Eigen::VectorXd b;
-    // radii of the wheels (m)
-    Eigen::VectorXd r;
-    // Min/max allowable value for steering angle, in rad.
-    Bounds beta_bounds;
-    // Min/max allowable value for rotation rate of modules, in rad/s
-    Bounds beta_dot_bounds;
-    // Min/max allowable value for the angular acceleration of the modules, in rad/s^2.
-    Bounds beta_2dot_bounds;
-    // Min/max allowable value for rotation rate of module wheels, in rad/s
-    Bounds phi_dot_bounds;
-    // Min/max allowable value for the angular acceleration of the module wheels, in rad/s^2.
-    Bounds phi_2dot_bounds;
+  //! Orthogonal projection onto the H-sphere's equator of steering x-axis basis vector (each column represents one module)
+  Eigen::MatrixXd a;
+  //! Orthogonal projection onto the H-sphere's equator of steering y-axis basis vector (each column represents one module)
+  Eigen::MatrixXd a_orth;
+  //! Distance from chassis centre to each module rotation axis, reported along the W axis (each column represents one module)
+  Eigen::MatrixXd l_v;
+
+  //! Array containing the angle to each of the modules measured counter clockwise from the x-axis in radians
+  Eigen::VectorXd alpha;
+  //! Discance from the centre of the robot to each module's steering axis, in m
+  Eigen::VectorXd l;
+  //! Horizontal distance from the axis of rotation of each module to its contact with the gound, in m
+  Eigen::VectorXd b;
+  //! Radii of the wheels, in m
+  Eigen::VectorXd r;
+  //! Min/max allowable value for steering angle, in rad.
+  Bounds beta_bounds;
+  //! Min/max allowable value for rotation rate of modules, in rad/s
+  Bounds beta_dot_bounds;
+  //! Min/max allowable value for the angular acceleration of the modules, in rad/s^2.
+  Bounds beta_2dot_bounds;
+  //! Min/max allowable value for rotation rate of module wheels, in rad/s
+  Bounds phi_dot_bounds;
+  //! Min/max allowable value for the angular acceleration of the module wheels, in rad/s^2.
+  Bounds phi_2dot_bounds;
 };
 
 }
--- include/libswervedrive/estimator.h
+++ include/libswervedrive/estimator.h
@@ -9,13 +9,37 @@
 #include "libswervedrive/chassis.h"
 
 namespace swervedrive {
+/**
+ * @brief Represents the derivatives of each steering angle with respect to
+ *        2/3 of u, v and w (the other has no value).
+ * 
+ */
 struct Derivatives {
   std::optional<Eigen::VectorXd> u, v, w;
 };
 
-
+/**
+ * @brief An object for estimating the ICR and rotation about it of a swerve drive.
+ * 
+ */
 class Estimator {
 public:
+  /**
+   * @brief Construct a new Estimator object
+   * 
+   * @param chassis a \sa{Chassis} object specifying the configuration of the chassis to estimate
+   * @param init Starting point [x, y, theta] of the chassis in the world frame. Defaults to zero.
+   * @param eta_lambda threshold when we assume the algorithm has converged to the correct
+   *     icr based on how much lambda has moved between sucessive iterations
+   * @param eta_delta threshold below which a starting point for iterative icr iteration is
+   *     selected based on the norm between the measured wheel positions and the wheel positions obtained from that starting point
+   * @param min_delta_line_search minimum size of the free parameters delta_m and delta_m
+   *     to avoid infinate recursion in the line search for the next iteration of the position of lambda
+   * @param max_iter_lambda maximum iterations allowed for the iterative icr estimation
+   *     algorithm to converge on one point
+   * @param singularity_tolerance how close a point must be to be considered to be
+   *     'on a structural singularity'
+   */
   Estimator(Chassis chassis,
     Epsilon init = Eigen::VectorXd::Zero(3, 1),
     double eta_lambda=1e-4,
@@ -26,34 +50,71 @@ public:
     );
   ~Estimator() = default;
 
-  // compute the derivatives of the constraining surface in the space of all possible wheel
-  // configurations at lambda
-  // returns (S_u, S_v, S_w), the vectors containing the derivatives of each steering angle
-  // with respect to u, v, and w respectively
+  /**
+   * @brief Compute derivatives of constraining surface at lambda.
+   * 
+   * @param lambda Position of ICR.
+   * @return Derivatives (S_u, S_v, S_w), the vectors containing the derivatives of each steering angle 
+   *     with respect to u, v, and w respectively
+   */
   Derivatives compute_derivatives(Lambda lambda);
+
+  // TODO document rest of methods as they are imelemented
+
+  /**
+   * @brief 
+   * 
+   * @return Lambda 
+   */
   Lambda estimate_lambda();
+  /**
+   * @brief 
+   * 
+   * @param lambda 
+   * @return int 
+   */
   int handle_singularities(Lambda lambda);
+  /**
+   * @brief 
+   * 
+   * @param lambda 
+   * @return Eigen::VectorXd 
+   */
   Eigen::VectorXd lambda_to_betas(Lambda lambda);
+  /**
+   * @brief 
+   * 
+   * @param q 
+   * @return std::vector<Lambda> 
+   */
   std::vector<Lambda> select_starting_points(Eigen::VectorXd q);
+  /**
+   * @brief 
+   * 
+   * @param derivatives 
+   * @param q 
+   * @param lambda 
+   * @return Eigen::Vector3d 
+   */
   Eigen::Vector3d solve(Derivatives derivatives, Eigen::VectorXd q, Lambda lambda);
+  /**
+   * @brief 
+   * 
+   * @param lambda 
+   * @param deltas 
+   * @param q 
+   * @return Lambda 
+   */
   Lambda update_parameters(Lambda lambda, Eigen::Vector3d deltas, Eigen::VectorXd q);
 
 protected:
   Chassis chassis_;
   Epsilon epsilon_;
 
-  // threshold when we assume the algorithm has converged to the correct icr based on how much
-  // lambda has moved between sucessive iterations
   double eta_lambda_ = 1e-4;
-  // threshold below which a starting point for iterative icr iteration is selected based on the norm
-  // between the measured wheel positions and the wheel positions obtained from that starting point
   double eta_delta_ = 1e-2;
-  // minimum size of the free parameters delta_m and delta_m to avoid infinate recursion in the line
-  // search for the next iteration of the position of lambda
   double min_delta_line_search_ = 1e-2;
-  // maximum iterations allowed for the iterative icr estimation algorithm to converge on one point
   double max_iter_lambda_ = 50;
-  // how close a point must be to be considered to be 'on a structural singularity'
   double singularity_tolerance_ = 1e-3;
 };
 }
