9f17d4d9459530dc60cf2cea80648bc6699c1b16
Arthur Allshire <arthur@allshire.org>
2019-04-20 21:37:19 -0400

    Improve lambda_to_betas tests

--- src/tests/test_estimator.cpp
+++ src/tests/test_estimator.cpp
@@ -6,7 +6,8 @@
 TEST(EstimatorTest, ConvertsLambdaToQ) {
   using namespace Eigen;
   using namespace swervedrive;
-  VectorXd alpha = VectorXd::Zero(4);
+  VectorXd alpha(4);
+  alpha << 0, M_PI/2, M_PI, 3.0*M_PI/2.0;
   VectorXd l = VectorXd::Constant(4, 1);
   VectorXd b = VectorXd::Constant(4, 1);
   VectorXd r = VectorXd::Constant(4, 1);
@@ -16,8 +17,22 @@ TEST(EstimatorTest, ConvertsLambdaToQ) {
       beta_bounds, beta_dot_bounds, beta_2dot_bounds, phi_dot_bounds, phi_2dot_bounds);
   Estimator e(c);
 
-  auto q = e.lambda_to_betas(Lambda(0, 0, 1));
+  Lambda lambda_spot_turn = Lambda(0, 0, 1);
+
+  auto q = e.lambda_to_betas(lambda_spot_turn);
   EXPECT_EQ(q.size(), 4);
   VectorXd expected = VectorXd::Zero(4);
-  EXPECT_TRUE(q.isApprox(expected)) << "Expected: " << expected << " Calculated: " << q;
+  EXPECT_TRUE(q.isApprox(expected)) << "Expected: " << expected << "\nCalculated: " << q;
+
+  // test an icr on a structural singularity
+  Lambda lambda_singular = Lambda(1, 0, 1).normalized();
+  q = e.lambda_to_betas(lambda_singular);
+  double expected_singular_wheel = M_PI/2.0;
+  EXPECT_NEAR(expected_singular_wheel, q(0), 1e-8)
+    << "Expected: " << expected_singular_wheel << "\nCalculated: " << q;
+
+  Lambda drive_along_x = Lambda(0, 1, 0);
+  q = e.lambda_to_betas(drive_along_x);
+  expected << M_PI/2.0, 0, M_PI/2.0, 0;
+  EXPECT_TRUE(q.isApprox(expected)) << "Expected: " << expected << "\nCalculated: " << q;
 }
