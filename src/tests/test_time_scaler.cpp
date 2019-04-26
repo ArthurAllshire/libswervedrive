7c2073fb492583fed588b453ce5db4e3b2517fd0
Arthur Allshire <arthur@allshire.org>
2019-04-25 22:12:54 -0400

    Finish writing time scaler tests

--- src/tests/test_time_scaler.cpp
+++ src/tests/test_time_scaler.cpp
@@ -10,7 +10,7 @@
 using namespace Eigen;
 using namespace swervedrive;
 
-class TimeScalerTest : public ChassisTest {
+class TimeScalerTest : public ::testing::Test {
 };
 
 // function to copy contained to a defined compound type (GenType)
@@ -33,20 +33,34 @@ double beta_bounds[2] = {-10., 10.};
 double phi_dot_bounds[2] = {-10., 10.};
 
 void assert_scaling_bounds(
-    Chassis& chassis,
     ModuleMotion module_motion,
     double beta_dot_bounds[2],
     double beta_2dot_bounds[2],
     double phi_2dot_bounds[2]
 )
 {
-  TimeScaler scaler(chassis);
+
+  int n = 4;
+  VectorXd alpha(n);
+  alpha << 0, M_PI / 2., M_PI, 3. / 2. * M_PI;  // On sides of square robot
+  VectorXd l = VectorXd::Constant(n, 1);        // Side length of robot 2m
+  VectorXd b = VectorXd::Constant(n, 0);        // No offset of wheels from axis
+  VectorXd r = VectorXd::Constant(n, 0.5);      // D=1m -> theta rad rotation gives theta metres displacement
 
   // first construct chassis
-  chassis.beta_bounds_ = generate_uniform_bounds(beta_bounds, chassis.n_);
-  chassis.beta_dot_bounds_ = generate_uniform_bounds(beta_dot_bounds, chassis.n_);
-  chassis.beta_2dot_bounds_ = generate_uniform_bounds(beta_2dot_bounds, chassis.n_);
-  chassis.phi_2dot_bounds_ = generate_uniform_bounds(phi_2dot_bounds, chassis.n_);
+  Chassis chassis(
+    alpha,
+    l,
+    b,
+    r,
+    generate_uniform_bounds(beta_bounds, n),
+    generate_uniform_bounds(beta_dot_bounds, n),
+    generate_uniform_bounds(beta_2dot_bounds, n),
+    generate_uniform_bounds(phi_dot_bounds, n),
+    generate_uniform_bounds(phi_2dot_bounds, n)
+  );
+
+  TimeScaler scaler(chassis);
 
 
   int lower_beta = module_motion.beta_dot < 0;
@@ -72,10 +86,12 @@ void assert_scaling_bounds(
       double ds_upper_sq = std::pow(bounds.first.upper, 2);
       EXPECT_TRUE(bounds.second.lower >=
         (beta_2dot_bounds[lower_beta]
-        - module_motion.beta_2dot * ds_upper_sq));
-      EXPECT_TRUE(bounds.second.upper >=
+        - module_motion.beta_2dot * ds_upper_sq) / module_motion.beta_dot
+        );
+      EXPECT_TRUE(bounds.second.upper <=
         (beta_2dot_bounds[upper_beta]
-        - module_motion.beta_2dot * ds_upper_sq));
+        - module_motion.beta_2dot * ds_upper_sq) / module_motion.beta_dot
+        );
   }
   if (!ignore_phi) {
       EXPECT_TRUE(bounds.first.upper >=
@@ -94,26 +110,82 @@ void assert_scaling_bounds(
 
   // then assert that it respects the motion
   for (int i = 0; i < chassis.n_; ++i) {
-      EXPECT_TRUE(beta_dot_bounds[0] <= motion[i].beta_dot) << "Violated lower beta_dot bound";
-      EXPECT_TRUE(motion[i].beta_dot <= beta_dot_bounds[1]) << "Violated upper beta_dot bound";
-      EXPECT_TRUE(beta_2dot_bounds[0] <= motion[i].beta_2dot) << "Violated lower beta_2dot bound";
-      EXPECT_TRUE(motion[i].beta_2dot <= beta_2dot_bounds[1]) << "Violated upper beta_2dot bound";
-      EXPECT_TRUE(phi_2dot_bounds[0] <= motion[i].phi_2dot) << "Violated lower phi_2dot bound";
-      EXPECT_TRUE(motion[i].phi_2dot <= phi_2dot_bounds[1]) << "Violated upper phi_2dot bound";
+      EXPECT_TRUE(beta_dot_bounds[0] <= scaled_motion[i].beta_dot) << "Violated lower beta_dot bound";
+      EXPECT_TRUE(scaled_motion[i].beta_dot <= beta_dot_bounds[1]) << "Violated upper beta_dot bound";
+      EXPECT_TRUE(beta_2dot_bounds[0] <= scaled_motion[i].beta_2dot) << "Violated lower beta_2dot bound";
+      EXPECT_TRUE(scaled_motion[i].beta_2dot <= beta_2dot_bounds[1]) << "Violated upper beta_2dot bound";
+      EXPECT_TRUE(phi_2dot_bounds[0] <= scaled_motion[i].phi_2dot) << "Violated lower phi_2dot bound";
+      EXPECT_TRUE(scaled_motion[i].phi_2dot <= phi_2dot_bounds[1]) << "Violated upper phi_2dot bound";
     }
 }
 
-TEST_F(TimeScalerTest, TestScalingBounds)
-{
-    double beta_dot[] = {-1 , 1};
-    double beta_2dot[] = {-1 , 1};
-    double phi_2dot[] = {-1 , 1};
-    ModuleMotion module_motion;
+double beta_dot[] = {-1 , 1};
+double beta_2dot[] = {-1 , 1};
+double phi_2dot[] = {-1 , 1};
+ModuleMotion module_motion;
 
+TEST_F(TimeScalerTest, TestPositiveInRange)
+{
     module_motion.beta_dot = 0.5;
     module_motion.beta_2dot = 0.25;
     module_motion.phi_2dot = 0.25;
 
-    assert_scaling_bounds(*chassis, module_motion, beta_dot, beta_2dot, phi_2dot);
-    
+    assert_scaling_bounds(module_motion, beta_dot, beta_2dot, phi_2dot);
+}
+
+TEST_F(TimeScalerTest, TestNegativeInRange)
+{
+    module_motion.beta_dot = -5;
+    module_motion.beta_2dot = -1.5;
+    module_motion.phi_2dot = -1.5;
+
+    assert_scaling_bounds(module_motion, beta_dot, beta_2dot, phi_2dot);
+}
+
+TEST_F(TimeScalerTest, TestDBetaZero)
+{
+    module_motion.beta_dot = 0.01;
+    module_motion.beta_2dot = -1.5;
+    module_motion.phi_2dot = -1.5;
+
+    assert_scaling_bounds(module_motion, beta_dot, beta_2dot, phi_2dot);
+}
+
+TEST_F(TimeScalerTest, TestD2BetaZero)
+{
+    module_motion.beta_dot = 5;
+    module_motion.beta_2dot = 0.01;
+    module_motion.phi_2dot = -1.5;
+
+    assert_scaling_bounds(module_motion, beta_dot, beta_2dot, phi_2dot);
+}
+
+TEST_F(TimeScalerTest, TestD2PhiZero)
+{
+    module_motion.beta_dot = 5;
+    module_motion.beta_2dot = -1.5 ;
+    module_motion.phi_2dot = 0;
+
+    assert_scaling_bounds(module_motion, beta_dot, beta_2dot, phi_2dot);
+
+}
+
+TEST_F(TimeScalerTest, TestOppositeSigns)
+{
+    module_motion.beta_dot = 5;
+    module_motion.beta_2dot = -1.5 ;
+    module_motion.phi_2dot = -5;
+
+    assert_scaling_bounds(module_motion, beta_dot, beta_2dot, phi_2dot);
+
+}
+
+TEST_F(TimeScalerTest, TestAllZero)
+{
+    module_motion.beta_dot = 0;
+    module_motion.beta_2dot = 0;
+    module_motion.phi_2dot = 0;
+
+    assert_scaling_bounds(module_motion, beta_dot, beta_2dot, phi_2dot);
+
 }
