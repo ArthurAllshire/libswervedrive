b8a995feaa4a00fe693d38899c8b982e0bd04811
Arthur Allshire <arthur@allshire.org>
2019-04-26 20:18:42 -0400

    Add bounds to chassis fixture, add stub kinematicmodel testing stuff

--- src/tests/chassis_fixture.h
+++ src/tests/chassis_fixture.h
@@ -2,11 +2,12 @@
 #define chassis_fixture_h
 
 #include "libswervedrive/chassis.h"
+#include "libswervedrive/kinematic_model.h"
 
 class ChassisTest : public ::testing::Test
 {
 protected:
-  void SetUp() override
+  virtual void SetUp() override
   {
     using namespace Eigen;
     using namespace swervedrive;
@@ -15,18 +16,57 @@ protected:
     VectorXd l = VectorXd::Constant(4, 1);        // Side length of robot 2m
     VectorXd b = VectorXd::Constant(4, 0);        // No offset of wheels from axis
     VectorXd r = VectorXd::Constant(4, 0.5);      // D=1m -> theta rad rotation gives theta metres displacement
+
+    double beta_bounds_arr[2] = {-10., 10.};
+    double phi_dot_bounds_arr[2] = {-10., 10.};
+    double beta_dot_bounds_arr[2] = {-1 , 1};
+    double beta_2dot_bounds_arr[2] = {-1 , 1};
+    double phi_2dot_bounds_arr[2] = {-1 , 1};
     Bounds beta_bounds, beta_dot_bounds, beta_2dot_bounds, phi_dot_bounds, phi_2dot_bounds;
+    for (int i = 0; i < 4; ++i) {
+      beta_bounds.push_back(Map<Vector2d>(beta_bounds_arr));
+      beta_dot_bounds.push_back(Map<Vector2d>(beta_dot_bounds_arr));
+      beta_2dot_bounds.push_back(Map<Vector2d>(beta_2dot_bounds_arr));
+      phi_dot_bounds.push_back(Map<Vector2d>(phi_dot_bounds_arr));
+      phi_2dot_bounds.push_back(Map<Vector2d>(phi_2dot_bounds_arr));
+    }
+
 
     chassis =
-        new Chassis(alpha, l, b, r, beta_bounds, beta_dot_bounds, beta_2dot_bounds, phi_dot_bounds, phi_2dot_bounds);
+        new Chassis(alpha, l, b, r,
+          beta_bounds, beta_dot_bounds, beta_2dot_bounds, phi_dot_bounds, phi_2dot_bounds
+        );
   }
 
-  void TearDown() override
+  virtual void TearDown() override
   {
     delete chassis;
   }
 
   swervedrive::Chassis* chassis;
+};
+
+class KinematicTest : public ChassisTest
+{
+protected:
+  void SetUp() override
+  {
+    using namespace swervedrive;
+
+    ChassisTest::SetUp();
+
+    kinematicmodel = new KinematicModel(*chassis, 1.);
+
+  }
+
+  void TearDown() override
+  {
+    ChassisTest::SetUp();
+    delete kinematicmodel;
+  }
+
+  swervedrive::KinematicModel* kinematicmodel;
+  
 };
 
 #endif
---
+++ src/tests/test_kinematic_model.cpp
@@ -0,0 +1,69 @@
+#include <gtest/gtest.h>
+
+#include "libswervedrive/chassis.h"
+#include "chassis_fixture.h"
+
+class TimeScalerTest : KinematicTest {
+};
+
+using namespace Eigen;
+using namespace swervedrive;
+
+TEST_F(KinematicTest, TestComputeActuatorMotion) {
+    // Lambda lambda;
+    // lambda << 0, 0, 1;
+    // Lambda lambda_dot;
+    // lambda_dot << 0, 0, 1;
+    // Lambda lambda_2dot;
+    // lambda_2dot << 0, 0, -1;
+    // double mu = 1.0;
+    // double mu_dot = 1.0;
+    // VectorXd betas;
+    // betas << 0, 0, 0, 0;
+
+
+    // TODO: figure out how to implement test - in original beta not included in call
+    /*
+    beta_prime, beta_2prime, phi_dot, phi_dot_prime = kinematic_model.compute_actuators_motion(
+        lmda, lmda_dot, lmda_2dot, mu, mu_dot
+    )
+
+    """
+    We do the calculations by hand to check that this is correct
+    Consider the first wheel
+    alpha = 0
+    a = [1 0 0]T
+    a_orth = [0 1 0]T
+    l = [0 0 1]T
+    sin(beta) = [0 1 0].[0 0 1]
+        = 0
+    cos(beta) = ([1 0 0]-[0 0 1]).[0 0 1]
+        = [1 0 -1].[0 0 1]
+        = -1
+
+    s1_lmda = sin(b)[1 0 -1] - cos(b)[0 1 0]
+        = [0 0 0] - [0 1 0]
+        = [0 -1 0]
+    s2_lmda = cos(b)[1 0 -1] + sin(b)[0 1 0]
+        = -1*[1 0 -1] + [0 0 0]
+        = [-1 0 1]
+    beta_prime = -[0 -1 0].[0 0 1]/[-1 0 1].[0 0 1]
+        = - 0/1
+        = 0
+    beta_2prime = (-2*0*[-1 0 1].[0 0 1]+[0 -1 0].[0 0 -1]) /
+        [-1 0 1].[0 0 1]
+        = (0+0)/1
+        = 0
+    phi_dot = ([-1 0 1]-[0 0 0]).[0 0 1]*1-0*0)/0.1
+        = 1/0.1
+        = 10
+    phi_dot_prime = ([-1 0 1]-[0 0 0]).([0 0 1]*1+[0 0 1]*1)-0*0)/0.1
+        = ([-1 0 1].[0 0 2])/0.1
+        = 20
+    """
+    assert np.isclose(beta_prime[0,0], 0.0, atol=1e-2)
+    assert np.isclose(beta_2prime[0,0], 0.0, atol=1e-2)
+    assert np.isclose(phi_dot[0,0], 10, atol=1e-2)
+    assert np.isclose(phi_dot_prime[0,0], 20, atol=1e-2)
+    */
+}
