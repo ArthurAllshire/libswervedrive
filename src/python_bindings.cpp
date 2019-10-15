64afde28e7b0940007d89aae7dd5d705c6991b01
James Ward <james@thedropbears.org.au>
2019-10-16 00:20:09 +0800

    Add more bindings to allow Python initialisation of chassis and controller

--- src/python_bindings.cpp
+++ src/python_bindings.cpp
@@ -7,33 +7,58 @@
 #include <vector>
 
 #include "libswervedrive/chassis.h"
+#include "libswervedrive/controller.h"
 #include "libswervedrive/estimator.h"
+#include "libswervedrive/kinematic_model.h"
+#include "libswervedrive/time_scaler.h"
 
 namespace py = pybind11;
 
 using namespace swervedrive;
 using Eigen::VectorXd;
+using std::vector;
 
 PYBIND11_MODULE(pyswervedrive, m)
 {
-  py::class_<Chassis>(m, "Chassis")
+  py::class_<Chassis> chassis(m, "Chassis");
+  chassis
       .def(py::init<VectorXd,  // alpha
                     VectorXd,  // l
-                    VectorXd,  // b
                     VectorXd   // r
                     >())
+      .def(py::init<VectorXd,  // alpha
+                    VectorXd,  // l
+                    VectorXd,  // r
+                    VectorXd   // b
+                    >())
       .def("betas", &Chassis::betas)
+      .def_readwrite("xi", &Chassis::xi_)
+      .def_readonly("state", &Chassis::state_)
       .def("__repr__", [](const Chassis& c) {
         std::stringstream ss;
         ss << "Chassis Object\n"
            << "Alpha:\n"
            << c.alpha_ << "\nl:\n"
-           << c.l_vector_ << "\nb:\n"
-           << c.b_ << "\nr:\n"
-           << c.r_;
+           << c.l_vector_ << "\nr:\n"
+           << c.r_ << "\nb:\n"
+           << c.b_;
         // TODO: Add more parameters, prettify printing
         return ss.str();
       });
+  py::enum_<Chassis::State>(chassis, "State")
+      .value("Stopping", Chassis::STOPPING)
+      .value("Reconfiguring", Chassis::RECONFIGURING)
+      .value("Running", Chassis::RUNNING)
+      .export_values();
+
+  py::class_<Controller>(m, "Controller")
+      .def(py::init<const Chassis&>(), py::arg("chassis"))
+      .def("controlStep", &Controller::controlStep,
+           "Return pairs of beta and phi_dot for desired x_dot, y_dot and theta_dot")
+      .def("updateStates", py::overload_cast<const VectorXd&, const VectorXd&>(&Controller::updateStates),
+           "Update the observed beta and phi_dot states")
+      .def("updateStates", py::overload_cast<const vector<ModuleState>&>(&Controller::updateStates),
+           "Update the observed beta and phi_dot states");
 
   py::class_<Estimator>(m, "Estimator")
       .def(py::init<const Chassis&, Xi, double, double, double, double, double>(), py::arg("chassis"),
