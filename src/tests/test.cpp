366743ac867cc79f0cff8584df3fd9a8955e0e12
James Ward <james@thedropbears.org.au>
2019-10-21 23:33:49 +0800

    Throw floating point exceptions in all tests

--- src/tests/test.cpp
+++ src/tests/test.cpp
@@ -1,7 +1,11 @@
 #include "gtest/gtest.h"
 
+#include <fenv.h>
+
 int main(int argc, char** argv)
 {
+  feenableexcept(FE_DIVBYZERO | FE_INVALID | FE_OVERFLOW);
+
   ::testing::InitGoogleTest(&argc, argv);
 
   return RUN_ALL_TESTS();
