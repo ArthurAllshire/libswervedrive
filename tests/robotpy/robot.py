22c1fd31d731ba27202ff7007c35fe8eb14796f8
James Ward <james@thedropbears.org.au>
2019-12-28 20:22:12 +1100

    Improve robotpy simulation

--- tests/robotpy/robot.py
+++ tests/robotpy/robot.py
@@ -17,13 +17,13 @@ class SwerveTestRobot(magicbot.MagicRobot):
 
     def createObjects(self):
         '''Create motors and stuff here'''
-        self.lr_swerve_drive_motor = ctre.WPI_TalonSRX(11)
-        self.rr_swerve_drive_motor = ctre.WPI_TalonSRX(21)
-        self.lf_swerve_drive_motor = ctre.WPI_TalonSRX(31)
+        self.lf_swerve_drive_motor = ctre.WPI_TalonSRX(11)
+        self.lr_swerve_drive_motor = ctre.WPI_TalonSRX(21)
+        self.rr_swerve_drive_motor = ctre.WPI_TalonSRX(31)
         self.rf_swerve_drive_motor = ctre.WPI_TalonSRX(41)
-        self.lr_swerve_steer_motor = ctre.WPI_TalonSRX(12)
-        self.rr_swerve_steer_motor = ctre.WPI_TalonSRX(22)
-        self.lf_swerve_steer_motor = ctre.WPI_TalonSRX(32)
+        self.lf_swerve_steer_motor = ctre.WPI_TalonSRX(12)
+        self.lr_swerve_steer_motor = ctre.WPI_TalonSRX(22)
+        self.rr_swerve_steer_motor = ctre.WPI_TalonSRX(32)
         self.rf_swerve_steer_motor = ctre.WPI_TalonSRX(42)
         self.lr_swerve_config = SwerveConfig(alpha=math.pi*3/4, l=1.414, r=0.05)
         self.rr_swerve_config = SwerveConfig(alpha=-math.pi*3/4, l=1.414, r=0.05)
@@ -31,6 +31,7 @@ class SwerveTestRobot(magicbot.MagicRobot):
         self.rf_swerve_config = SwerveConfig(alpha=-math.pi*1/4, l=1.414, r=0.05)
 
         self.joystick = wpilib.Joystick(0)
+        self.t = 0.0
 
     def teleopInit(self):
         '''Called when teleop starts; optional'''
@@ -43,6 +44,14 @@ class SwerveTestRobot(magicbot.MagicRobot):
         vy = -self.joystick.getX() * 3.0
         vz = -self.joystick.getZ() * 0.5  # rad/s
 
+        vx = vy = vz = 0.0
+        import math
+        #vx = math.sin(self.t/100.0)
+        #vy = math.cos(self.t/100.0)
+        #vx = int(self.t/100.0) % 3 - 1
+        #vy = int(self.t/100.0 + 1) % 3 - 1
+        self.t += 1
+        vz = 0.5
         #self.chassis.drive(vx, vy, vz)
         self.chassis.icr_drive(vx, vy, vz)
 
